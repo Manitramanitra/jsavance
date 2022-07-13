836d5e9bd75c949f6eefcff7ac0a8f8661b2191c
Manitramanitra <randiluc@gmail.com>
2022-07-13 14:41:56 +0300

    test

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-const lastname = document.querySelector('#lastname')
-const firstname = document.querySelector('#firstname')
-const email = document.querySelector('#email')
-const bouton = document.querySelector('#formulaire')
+// const lastname = document.querySelector('#lastname')
+// const firstname = document.querySelector('#firstname')
+// const email = document.querySelector('#email')
+// const bouton = document.querySelector('#formulaire')
 
-function user(
+// function user(
+//     {
+//         nom,
+//         prenom,
+//         Email,
+//         id=Math.floor(Math.random()*new Date())
+//     })
+// {
+//     this.nom = nom;
+//     this.prenom = prenom;
+//     this.Email= Email;
+//     this.id=id;
+// }
+
+// bouton.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+//     const moi = new user({
+//         Email:email.value,
+//         nom:lastname.value,
+//         prenom:firstname.value,
+//     } )
+//     console.log(moi)
+// })
+const tab = [1,3,3,6]
+tab.forEach((element,index) => {
+    if(index!=tab.length-1)
     {
-        nom,
-        prenom,
-        Email,
-        id=Math.floor(Math.random()*new Date())
-    })
-{
-    this.nom = nom;
-    this.prenom = prenom;
-    this.Email= Email;
-    this.id=id;
-}
-
-bouton.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const moi = new user({
-        Email:email.value,
-        nom:lastname.value,
-        prenom:firstname.value,
-    } )
-    console.log(moi)
-})
+        // console.log(element);
+        // console.log(tab[index+1])
+    }
+});
+const tab2 = ["rova","fifaliana",...tab]
+console.log(tab2)
